b3bf13dce7273f659f40252c83e0522ccc768d61
yousef <yousef.yaser@gmail.com>
2024-08-28 14:32:19 +0300

    add commons module and edit the revision

---
+++ commons/src/main/java/com/common/enums/TopicNames.java
@@ -0,0 +1,20 @@
+package com.common.enums;
+
+public enum TopicNames {
+    CUSTOMER_NEW("customer.new"),
+    CUSTOMER_UPDATE("customer.update"),
+    CUSTOMER_DELETE("customer.delete"),
+    ACCOUNT_NEW("account.new"),
+    ACCOUNT_UPDATE("account.update"),
+    ACCOUNT_DELETE("account.delete");
+
+    private final String topicName;
+
+    TopicNames(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+}
